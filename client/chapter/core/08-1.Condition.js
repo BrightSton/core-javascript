a284e76973be0389105381dea21a8ffe1ec86e10
LeeHuynSuk <gusehf321@naver.com>
2022-12-29 13:15:31 +0900

    [수정] if문과 삼항연산자 학습

--- client/chapter/core/08-1.Condition.js
+++ client/chapter/core/08-1.Condition.js
@@ -14,17 +14,29 @@ console.log(answer);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = "yes";
 
 // 영화 볼거니?
-let goingToWatchMovie;
-
-// if 문(statement)
-
-// else 절(caluse)
-
-// else if 복수 조건 처리
+let goingToWatchMovie = "no";
 
 // 조건부 연산자
+// if 문(statement)
+if (didWatchMovie === "yes") {
+  console.log("그 영화 재미있더라");
+} else if (goingToWatchMovie.includes("yes")) {
+  // else if 복수 조건 처리
+  console.log("그 영화 엄청 기대하구 있어");
+} else {
+  // else 절(caluse)
+  console.log("그 영화 재미없다구 해서 안보려구");
+}
 
 // 멀티 조건부 연산자 식
+let answer =
+  didWatchMovie === "yes"
+    ? "그 영화 재미있더라"
+    : goingToWatchMovie === "yes"
+    ? "그 영화 엄청 기대하구 있어"
+    : "그 영화 재미없다구 해서 안보려구";
+
+console.log(answer);
