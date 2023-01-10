511542c75289f78c05fc9bce2380acf11e97d49d
LeeHuynSuk <gusehf321@naver.com>
2023-01-10 22:00:33 +0900

    [추가] 돔 조작 학습

--- client/chapter/DOM/04.manipulation.js
+++ client/chapter/DOM/04.manipulation.js
@@ -47,39 +47,7 @@ let index = 1;
 let template =
   /* html */
   `<div class='box${index}'>새롭게 추가된 엘리먼트 입니다.</div>`;
-
-function insertBefore(node, text) {
-  if (typeof node === "string") {
-    node = getNode(node);
-  }
-  node.insertAdjacentHTML("befoerbegin", text);
-}
-
-function insertFirst(node, text) {
-  if (typeof node === "string") {
-    node = getNode(node);
-  }
-  node.insertAdjacentHTML("afterbegin", text);
-}
-
-function insertLast(node, text) {
-  if (typeof node === "string") {
-    node = getNode(node);
-  }
-  if (node.nodeType !== document.ELEMENT_NODE) {
-    throw new TypeError(
-      "insertBefore 함수의 첫 번째 인자는 ELEMENT_NODE 여야 합니다."
-    );
-  }
-  node.insertAdjacentHTML("beforeend", text);
-}
-
-function insertAfter(node, text) {
-  if (typeof node === "string") {
-    node = getNode(node);
-  }
-  node.insertAdjacentHTML("afterend", text);
-}
+// h1.insertAdjacentHTML('afterend',template)
 
 insertBefore("h1", template);
 
