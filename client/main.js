e535131831cbf632b67eb48d9b7cb4d060aa383b
LeeHuynSuk <gusehf321@naver.com>
2023-01-13 10:39:44 +0900

    [추가] 계산기 만들기

---
+++ client/main.js
@@ -0,0 +1,46 @@
+// 1. 인풋 벨류값 가져오기
+// 2. 이벤트 핸들러 연결하기
+// 3. 이벤트 기본 동작 차단하기
+// 4. 두 수의 합을 더해주기
+// 5. 화면에 출력하기
+
+/* const firstNumber = getNode("#firstNumber");
+const secondNumber = getNode("#secondNumber");
+const result = getNode(".result");
+const done = getNode("#done");
+
+done.addEventListener("click", function (e) {
+  e.preventDefault();
+  let sum = +firstNumber.value + +secondNumber.value;
+  result.textContent = sum;
+}); */
+
+const firstInput = getNode("#firstNumber");
+const secondInput = getNode("#secondNumber");
+const done = getNode("#done");
+
+function getInputValue(node) {
+  if (typeof node === "string") node = getNode(node);
+  if (node.tagName !== "INPUT")
+    refError("getInputValue 함수는  INPUT ELEMENT만 허용합니다.");
+  return node.value;
+}
+
+const sum = (valueA, valueB) => valueA + valueB;
+
+const clearContent = (node) => {
+  if (typeof node === "string") node = getNode(node);
+  node.textContent = "";
+};
+
+function handler(e) {
+  e.preventDefault();
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  let total = sum(firstValue, secondValue);
+
+  clearContent(".result");
+  insertLast(".result", total);
+}
+
+done.addEventListener("click", handler);
