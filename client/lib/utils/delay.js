8fd81d54cbfdcea0ef575bcfadd26433e594a588
LeeHuynSuk <gusehf321@naver.com>
2023-01-18 11:11:43 +0900

    [추가] delayP 함수 만들기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,4 +1,5 @@
 import { getNode } from "../dom/getNode.js";
+import { isNumber, isObject } from "./typeOf.js";
 
 const first = getNode(".first");
 
@@ -15,3 +16,40 @@ delay(() => {
     });
   });
 });
+
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data: "성공했습니다.",
+  errorMessage: "알 수 없는 오류가 발생했습니다.",
+};
+
+export function delayP(options = {}) {
+  let config = { ...defaultOptions }; // ->Object.assign({},defaultOptions)
+
+  if (isNumber(options)) {
+    config.timeout = options;
+  }
+
+  // 객체 합성 mixin
+  if (isObject(options)) {
+    config = { ...config, ...defaultOptions };
+  }
+
+  const { shouldReject, data, errorMessage, timeout } = config;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+    }, timeout);
+  });
+}
+
+delayP({
+  shouldReject: false,
+  timeout: 1000,
+  data: "  찐성공",
+  errorMessage: "오류 발생",
+})
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
